9e94473e4c5c0105464facb1deb32f07b63a6231
Nicolas Artaza <fliaartaza@gmail.com>
2022-11-17 22:59:50 -0300

    mmodificaciones generales (fetch, asincronia, estetica, etc)

--- js/main.js
+++ js/main.js
@@ -5,40 +5,62 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-//INYECTAR EL HTML
-stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-    <img src=${producto.img} alt= "">
-    <h3>${producto.nombre}</h3>
-    <p class="precioProducto">Precio:<span class="el-precio-color">$${producto.precio}</span></p>
-    <button id="agregar${producto.id}" class="boton-agregar">Agregar +</button>
-    `
-    contenedorProductos.appendChild(div)
 
-    const boton = document.getElementById(`agregar${producto.id}`)
-    //Por cada elemento de mi array, creo un div, lo cuelgo, le pongo un id particular, una vez colgado le hago un getElementById. Obtengo el elemento y a dicho elemento le agrego el evento
+// Traer del json los el array de objetos con un una funcion asincronica
+const elStockCompleto = async () => {
+    const response = await
+        fetch('/productos.json')
+    const data = await response.json()
 
-    boton.addEventListener('click', () => {
-        //Esta funcion se ejecuta el agregar el carrito con la id del producto
-        agregarAlCarrito(producto.id)
+    data.forEach((producto) => {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src=${producto.img} alt= "">
+        <h3>${producto.nombre}</h3>
+        <p class="precioProducto">Precio: <span class="el-precio-color">$${producto.precio}</span></p>
+        <button id="agregar${producto.id}" class="boton-agregar">Agregar +</button>
+        `
+        contenedorProductos.appendChild(div)
+
+        const boton = document.getElementById(`agregar${producto.id}`)
+        //Por cada elemento de mi array, creo un div, lo cuelgo, le pongo un id particular, una vez colgado le hago un getElementById. Obtengo el elemento y a dicho elemento le agrego el evento
+
+        boton.addEventListener('click', () => {
+            //Esta funcion se ejecuta el agregar el carrito con la id del producto
+            agregarAlCarrito(producto.id)
+
+            Toastify({
+                text: "¡Producto agregado al carrito con exito!",
+                duration: 1500,
+                newWindow: true,
+                gravity: "bottom", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                    background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(14,25,90,1) 10%, rgba(65,105,225,1) 68%, rgba(47,96,243,1) 86%)",
+                },
+                className: "toast-compra",
+                onClick: function () { } // Callback after click
+            }).showToast();
+
+        })
     })
-})
+}
+elStockCompleto()
+
 
 const actualizarCarrito = () => {
     contenedorCarrito.innerHTML = ""
-    localStorage.clear()
-    //Va a borrar los datos del carrito en localStorage
 
-    //Por cada producto se crea un div con la misma estructura y la agrego al contenedorCarrito (el modal)
+    //Por cada producto se crea un div con esta estructura y la agrego al contenedorCarrito (el modal)
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
         div.innerHTML = `
         <p>${prod.nombre}</p>
-        <p>Precio:$${prod.precio}</p>
-        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <p>$${prod.precio}</p>
+        <p><span id="cantidad">${prod.cantidad}</span></p>
         <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class='bx bxs-trash'></i></button>
         `
 
@@ -60,16 +82,29 @@ const agregarAlCarrito = (prodId) => {
 
     if (existe) { //Si ya esta en el carro, actualiza la cantidad
         const prod = carrito.map(prod => { //map encuentra los que son iguales y actualiza la cantidad
-            if (prod.id === prodId) {
-                prod.cantidad++
-            }
+
+            // // IF CLASICO
+            // if (prod.id === prodId) {
+            //     prod.cantidad++
+            // }
+
+            // // AND
+            prod.id === prodId && prod.cantidad++
         })
     } else {
-        const item = stockProductos.find((prod) => prod.id === prodId)
-        //Si no está, se agrega 
-        carrito.push(item)
-    }
+        const elStockCompleto = async () => {
+            const response = await
+                fetch('/productos.json')
+            const data = await response.json()
 
+            const item = data.find((prod) => prod.id === prodId)
+            //Si no está, se agrega 
+            carrito.push(item)
+            actualizarCarrito()
+        }
+        // llamo a la funcion
+        elStockCompleto()
+    }
     //Se actualiza el carrito
     actualizarCarrito()
 }
@@ -84,17 +119,72 @@ const eliminarDelCarrito = (prodId) => {
 
     actualizarCarrito()
 
+    Toastify({
+        text: "Producto/s eliminado/s",
+        duration: 1500,
+        newWindow: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,68,1) 0%, rgba(255,46,0,1) 100%)",
+        },
+        className: "toast-compra",
+        onClick: function () { } // Callback after click
+    }).showToast();
+
     console.log(carrito)
 }
 
 
 botonVaciar.addEventListener('click', () => {
+
+    if (carrito.length !== 0) { //para que no aparezca el toast sin que haya productos para vaciar el carrito
+        Toastify({
+            text: "Carrito vaciado",
+            duration: 1500,
+            newWindow: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,68,1) 0%, rgba(255,46,0,1) 100%)",
+            },
+            className: "toast-compra",
+            onClick: function () { } // Callback after click
+        }).showToast();
+    }
+
     carrito.length = 0
+
+    localStorage.clear()
+    //Va a borrar los datos del carrito en localStorage
+
     actualizarCarrito()
+
+    contenedorModal.classList.toggle('modal-active')
+    // para que se cierre el modal después de vaciar el carrito
 })
+
 botonComprar.addEventListener("click", () => {
+    if (carrito.length !== 0) {
+        Swal.fire({
+            title: 'Kits!',
+            text: '¡Muchas gracias por la compra, vuelva pronto!.',
+            imageUrl: 'https://cdn.freebiesupply.com/logos/thumbs/1x/evgord-page-logo.png',
+            imageWidth: 200,
+            imageHeight: 200,
+            imageAlt: 'Logo de Kits!',
+        })
+    }
+
     carrito.length = 0
+
+    localStorage.clear()
+
     actualizarCarrito()
+
+    contenedorModal.classList.toggle('modal-active')
 })
 
 
--- js/variables.js
+++ js/variables.js
@@ -1,13 +1,12 @@
 const contenedorProductos = document.getElementById('contenedor-productos')
 
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-
 const botonVaciar = document.getElementById('vaciar-carrito')
 const botonComprar = document.getElementById('submit')
 
 const contadorCarrito = document.getElementById('contadorCarrito')
 
-const cantidad = document.getElementById('cantidad')
+const cantidad = document.getElementById('#cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
 
